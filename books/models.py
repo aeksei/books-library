f87002ec59b9f7b541cf29c03cc3ca6a013e6a1c
aeksei <aeksei94@gmail.com>
2024-03-22 15:45:07 +0300

    feat: create Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class TypeChoices(models.TextChoices):
+        HARDCOVER = "HB", "Твёрдый переплёт"
+        PAPERCOVER = "PB", "Мягкий переплёт"
+        EBOOK = "EB", "Электроная книга"
+
+    title = models.CharField(max_length=100)
+    year = models.PositiveSmallIntegerField()
+    description = models.TextField(default="")
+    book_type = models.CharField(max_length=2, choices=TypeChoices.choices, default=TypeChoices.PAPERCOVER)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
