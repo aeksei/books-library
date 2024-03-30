46740c1055481e7ad09ae85178d901bf212fed09
aeksei <aeksei94@gmail.com>
2024-03-30 11:29:46 +0300

    feat: настроено отображение связей many-to-one, one-to-many, many-to-many в админке

--- books/admin.py
+++ books/admin.py
@@ -5,12 +5,22 @@ from books import models
 
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
-    ...
+    filter_horizontal = ("tags",)
+    autocomplete_fields = ("category",)
+
+
+class BookInline(admin.StackedInline):
+    model = models.Book
+    fields = ("title",)
+    readonly_fields = ("title",)
 
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    ...
+    search_fields = ("name",)
+    inlines = [
+        BookInline,
+    ]
 
 
 @admin.register(models.Tag)
