8f2fcbb46814b24272f6c6cd6dcee8941ca50111
aeksei <aeksei94@gmail.com>
2024-04-08 21:02:10 +0300

    refactoring: используем generic для REST API вместо самописного кода

--- books_api/views.py
+++ books_api/views.py
@@ -5,80 +5,19 @@ from django.shortcuts import get_object_or_404
 
 from books import models
 from books_api import serializers
+from rest_framework import generics
 
 
-class ListCreateAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        """Список книг - /books/"""
-        queryset = models.Book.objects.all()
-
-        serializer = serializers.BookSerializer(
-            instance=queryset, many=True,
-        )
-
-        return Response(data=serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        serializer = serializers.BookSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                data=serializer.data,
-                status=status.HTTP_201_CREATED,
-            )
-
-        return Response(
-            data=serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+class ListCreateBookAPIView(generics.ListCreateAPIView):
+    queryset = models.Book.objects.all()
+    serializer_class = serializers.BookSerializer
 
 
-class RetriveUpdateDeleteBookAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        # pk = kwargs["pk"] заменено на **kwargs в get_object_or_404
-        obj = get_object_or_404(models.Book, **kwargs)
-
-        serializer = serializers.BookSerializer(obj)
-
-        return Response(serializer.data)
-
-    def put(self, request, *args, **kwargs):
-        obj = get_object_or_404(models.Book, **kwargs)
-
-        serializer = serializers.BookSerializer(instance=obj, data=request.data)
-        if serializer.is_valid():
-            serializer.save()  # Обновление
-            return Response(
-                data=serializer.data,
-                status=status.HTTP_200_OK,
-            )
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RetriveUpdateDeleteBookAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Book.objects.all()
+    serializer_class = serializers.BookSerializer
 
 
-class ListCreateCategoryAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        """Список книг - /books/"""
-        queryset = models.Category.objects.all()
-
-        serializer = serializers.CategorySerializer(
-            instance=queryset, many=True,
-        )
-
-        return Response(data=serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        serializer = serializers.CategorySerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                data=serializer.data,
-                status=status.HTTP_201_CREATED,
-            )
-
-        return Response(
-            data=serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+class ListCreateCategoryAPIView(generics.ListCreateAPIView):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
