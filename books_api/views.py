e6c48a84f6a12a567ac212d8400062fca3059904
aeksei <aeksei94@gmail.com>
2024-04-14 20:00:47 +0400

    feat: использование фильтров в DRF

--- books_api/views.py
+++ books_api/views.py
@@ -6,11 +6,14 @@ from django.shortcuts import get_object_or_404
 from books import models
 from books_api import serializers
 from rest_framework import generics
+from django_filters import rest_framework as filters
 
 
 class ListCreateBookAPIView(generics.ListCreateAPIView):
     queryset = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('year', 'book_type')
 
 
 class RetriveUpdateDeleteBookAPIView(generics.RetrieveUpdateDestroyAPIView):
